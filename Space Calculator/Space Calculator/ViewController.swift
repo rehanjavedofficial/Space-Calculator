46b0870bbda1347e1cfb2754034b45bcb4c39b62
Rehan Javed <rehanjaved.official@gmail.com>
2018-02-10 23:53:45 +0500

    Sounds in Buttons
    Added sound to all buttons when they pressed.

--- Space Calculator/Space Calculator/ViewController.swift
+++ Space Calculator/Space Calculator/ViewController.swift
@@ -7,19 +7,114 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var sound: AVAudioPlayer!
+    
+    /**
+     * Main method which executes when program will loads.
+    */
     override func viewDidLoad() {
+    
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+        let path = Bundle.main.path(forResource: "click", ofType: "wav")
+        let url = URL(fileURLWithPath: path!)
+        
+        do{
+            
+            try sound = AVAudioPlayer(contentsOf: url)
+            sound.prepareToPlay()
+            
+        }catch let exception as NSError{
+            print(exception.debugDescription)
+        }
+        
+    }
+    
+    /**
+     * When number button is pressed.
+     */
+    @IBAction func numberPressedInCalculator(sender: UIButton){
+        
+        playSound()
+        
+    }
+    
+    /**
+     * To play sound when button pressed.
+     */
+    private func playSound(){
+    
+        if sound.isPlaying{
+            sound.stop()
+        }
+        
+        sound.play()
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func clearButton(_ sender: Any) {
+        
+        playSound()
+        
     }
-
-
+    
+    @IBAction func signChanger(_ sender: Any) {
+        
+        playSound()
+        
+    }
+    
+    @IBAction func resetBtn(_ sender: Any) {
+        
+        playSound()
+        
+    }
+    
+    @IBAction func signChange(_ sender: Any) {
+        
+        playSound()
+        
+    }
+    
+    @IBAction func percentageBtn(_ sender: Any) {
+        
+        playSound()
+        
+    }
+    
+    @IBAction func divideBtn(_ sender: Any) {
+        
+        playSound()
+        
+    }
+    
+    @IBAction func multiplyBtn(_ sender: Any) {
+        
+        playSound()
+        
+    }
+    
+    @IBAction func addBtn(_ sender: Any) {
+        
+        playSound()
+        
+    }
+    
+    @IBAction func subtractBtn(_ sender: Any) {
+        
+        playSound()
+        
+    }
+    
+    @IBAction func equalBtn(_ sender: Any) {
+        
+        playSound()
+        
+    }
+    
 }
 
