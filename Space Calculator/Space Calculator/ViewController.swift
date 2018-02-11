e21f25a3a48427b5dd37c92a8651fbfa5172d0ed
Rehan Javed <rehanjaved.official@gmail.com>
2018-02-11 23:31:25 +0500

    Add Calculation Functions
    Add Calculation Functions added Successfully.

--- Space Calculator/Space Calculator/ViewController.swift
+++ Space Calculator/Space Calculator/ViewController.swift
@@ -11,7 +11,17 @@ import AVFoundation
 
 class ViewController: UIViewController {
 
-    var sound: AVAudioPlayer!
+    @IBOutlet weak var display: UILabel!
+    private var sound: AVAudioPlayer!
+    private var firstNumber = "0", secondNumber: String = "0"
+    private enum Operater{
+        case MULTIPLY
+        case DIVIDE
+        case ADD
+        case SUBTRACT
+        case NOTHING
+    }
+    private var selectedOperator:Operater = .NOTHING
     
     /**
      * Main method which executes when program will loads.
@@ -41,6 +51,20 @@ class ViewController: UIViewController {
         
         playSound()
         
+        if selectedOperator == Operater.NOTHING{
+            if firstNumber == "0"{
+                firstNumber = ""
+            }
+            firstNumber += String(sender.tag)
+            display.text = firstNumber
+        }else{
+            if secondNumber == "0"{
+                secondNumber = ""
+            }
+            secondNumber += String(sender.tag)
+            display.text = secondNumber
+        }
+        
     }
     
     /**
@@ -56,21 +80,14 @@ class ViewController: UIViewController {
         
     }
 
-    @IBAction func clearButton(_ sender: Any) {
-        
-        playSound()
-        
-    }
-    
-    @IBAction func signChanger(_ sender: Any) {
-        
-        playSound()
-        
-    }
-    
     @IBAction func resetBtn(_ sender: Any) {
         
         playSound()
+        
+        firstNumber = "0"
+        secondNumber = "0"
+        display.text = "0"
+        selectedOperator = Operater.NOTHING
         
     }
     
@@ -78,11 +95,28 @@ class ViewController: UIViewController {
         
         playSound()
         
+        if selectedOperator == .NOTHING{
+            
+            var percentageResult = Double(firstNumber)!
+            percentageResult *= -1
+            firstNumber = String(percentageResult)
+            display.text = firstNumber
+    
+        }
+        
     }
     
     @IBAction func percentageBtn(_ sender: Any) {
         
         playSound()
+        
+        if selectedOperator == .NOTHING{
+            
+            let percentageResult = Double(firstNumber)! / 100
+            firstNumber = String(percentageResult)
+            display.text = firstNumber
+            
+        }
         
     }
     
@@ -90,11 +124,17 @@ class ViewController: UIViewController {
         
         playSound()
         
+        selectedOperator = .DIVIDE
+        display.text = "0"
+        
     }
     
     @IBAction func multiplyBtn(_ sender: Any) {
         
         playSound()
+        
+        selectedOperator = .MULTIPLY
+        display.text = "0"
         
     }
     
@@ -102,17 +142,63 @@ class ViewController: UIViewController {
         
         playSound()
         
+        selectedOperator = .ADD
+        display.text = "0"
+        
     }
     
     @IBAction func subtractBtn(_ sender: Any) {
         
         playSound()
         
+        selectedOperator = .SUBTRACT
+        display.text = "0"
+        
     }
     
     @IBAction func equalBtn(_ sender: Any) {
         
         playSound()
+        
+        let doubleFirstNumber = Double(firstNumber)
+        let doubleSecondNumber = Double(secondNumber)
+        let result: Double
+        
+        switch(selectedOperator)
+        {
+        case .MULTIPLY:
+            result = doubleFirstNumber! * doubleSecondNumber!
+        case .DIVIDE:
+            result = doubleFirstNumber! / doubleSecondNumber!
+        case .ADD:
+            result = doubleFirstNumber! + doubleSecondNumber!
+        case .SUBTRACT:
+            result = doubleFirstNumber! - doubleSecondNumber!
+        default:
+            result = 0
+        }
+        
+        display.text = String(result)
+        firstNumber = "0"
+        secondNumber = "0"
+        selectedOperator = .NOTHING
+        
+    }
+    
+    /**
+     * Throwing message on the screen.
+    */
+    private func throwMessage(message msg: String){
+        
+        let alert = UIAlertController(title: nil, message: msg, preferredStyle: .alert)
+        self.present(alert, animated: true)
+        
+        // duration in seconds
+        let duration: Double = 2
+        
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + duration) {
+            alert.dismiss(animated: true)
+        }
         
     }
     
